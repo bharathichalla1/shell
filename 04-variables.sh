ef7e0614ebeacb2021b26fd6de28a68c8562e1e4
bharathichalla1 <challabharathi1994@gmail.com>
2024-11-05 14:56:56 +0530

    fourth script

--- 04-variables.sh
+++ 04-variables.sh
@@ -3,6 +3,8 @@
 person1=$1
 person2=$2
 
+#we have to pass arugments from outside also.  eg: sh 04-variables.sh pradeep bharathi
+
 echo "$person1 : hello $person2"
 echo "$person2 : hi $person1"
 echo "$person1 : how are you $person2"
