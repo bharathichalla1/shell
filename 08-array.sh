d3ef207f0c49ebc31a1e2587dd522fcae07bc1b7
bharathichalla1 <challabharathi1994@gmail.com>
2024-11-06 11:58:46 +0530

    eighth script

--- 08-array.sh
+++ 08-array.sh
@@ -5,4 +5,4 @@ FRUITS=("Apple" "Banana" "Mango")
 echo "first value: ${FRUITS[0]}"
 echo "second value : ${FRUITS[1]}"
 echo "third value : ${FRUITS[2]}"
-echo "all fruits : ${FRUITS[@]}
+echo "all fruits : ${FRUITS[@]}"
