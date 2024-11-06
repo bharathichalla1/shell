5d3e8a56d2606d996a74738b73ed350a2cac59fd
bharathichalla1 <challabharathi1994@gmail.com>
2024-11-06 15:59:05 +0530

    13th script

--- 13-logs.sh
+++ 13-logs.sh
@@ -3,6 +3,10 @@
 ID=$(id -u)
 
 TIMESTAMP=$(date +F-%H-%M-%S)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 LOG_FILE="/tmp/$0-$TIMESTAMP.log" #$0 means script name 
 
@@ -10,10 +14,10 @@ LOG_FILE="/tmp/$0-$TIMESTAMP.log" #$0 means script name
 VALIDATE(){
     if [ $1 -ne 0 ]
   then 
-       echo "ERROR : $2..failed"
+       echo -e "ERROR : $2..$R failed $N"
        exit 1
   else
-       echo "$2..success"
+       echo -e "$2..$G success $N"
     fi   
 }
 
@@ -21,7 +25,7 @@ VALIDATE(){
 
 if [ $ID -ne 0 ]
 then
-  echo "ERROR : please run the script with root user"
+  echo -e  "$R ERROR : please run the script with root user $N"
   exit 1 # it is EXIT STATUS to check the above command is success or not (to check you can enter echo $? if it is 0 means success, other than 0 means failure), you can give other than 0
 else
   echo "you are root user"
