e3e34362fa33720154d1096f1224c8691da36920
bharathichalla1 <challabharathi1994@gmail.com>
2024-11-06 13:03:33 +0530

    tenth script

--- 10-install-mysql.sh
+++ 10-install-mysql.sh
@@ -5,10 +5,18 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then
   echo "ERROR : please run the script with root user"
-  exit 1 # you can give other than 0
+  exit 1 # it is EXIT STATUS to check the above command is success or not (to check you can enter echo $? if it is 0 means success, other than 0 means failure), you can give other than 0
 else
   echo "you are root user"
 fi    
 
 
 yum install mysql -y
+
+if [ $? -ne 0 ]
+then 
+  echo "ERROR : installing mysql is failed"
+  exit 1
+else
+  echo "installing mysql is success"
+fi  
