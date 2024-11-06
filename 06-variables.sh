e76fd4ef409659e774d921c3f42cd2978d84f448
bharathichalla1 <challabharathi1994@gmail.com>
2024-11-06 10:40:50 +0530

    SIXTH script

--- 06-variables.sh
+++ 06-variables.sh
@@ -3,5 +3,12 @@
 echo "please enter username :"
 
 read -s USERNAME #the value entered above will be automatically attached to USERNAME variable
+ 
+echo "username is : $USERNAME, password is : $PASSWORD" # priniting just for validation, should not print username and passowrds in scripts
 
-echo "username is : $USERNAME"
+echo "please enter password :"
+
+read -s PASSWORD
+
+
+
