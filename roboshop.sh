1a22dd4f913d0d96edf53ce606c8e2d4a60770c6
bharathichalla1 <challabharathi1994@gmail.com>
2024-11-08 19:35:43 +0530

    instancescreating

--- roboshop.sh
+++ roboshop.sh
@@ -16,8 +16,8 @@ do
 
 
     IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-047ba035b5558d59f  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'instances[0].PrivateIpAddress' --output text)
+    
     echo "$i: $IP_ADDRESS"
-
 
 done
 
