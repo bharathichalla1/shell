ecc8a5487b4107414360e067bc1db679a92c96aa
bharathichalla1 <challabharathi1994@gmail.com>
2024-11-08 20:03:48 +0530

    instancescreating

--- roboshop.sh
+++ roboshop.sh
@@ -12,12 +12,14 @@ do
         INSTANCE_TYPE="t3.small"
     else
         INSTANCE_TYPE="t2.micro"
-    fi    
+    fi 
 
-
-    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-047ba035b5558d59f  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'instances[0].PrivateIpAddress' --output text)
     
-    echo "instance is  : $i"
+    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCE_TYPE --security-group-ids sg-047ba035b5558d59f --tag-specifications"ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'instances[0].PrivateIpAddress' --output text)
+    
+   
+    
+    echo "$i: $IP_ADDRESS"
 
 done
 
